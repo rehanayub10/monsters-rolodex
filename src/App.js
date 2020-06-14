9060d4b6ba5bbd906d2706a7d72ae04d574d3e6f
Rehan Ayub <rehanayub20@gmail.com>
2020-06-14 11:37:58 -0400

    Added layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+     monsters: [],
+     searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="search" placeholder="Search Monsters..." onChange={
+          e => {this.setState({ searchField: e.target.value});
+          // console.log(this.state.searchField); //pass as callback
+        }
+          }></input>
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './card.styles.css';
+
+export const Card = (props) => (
+    <div className='card-container'>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}.png?set=set2&size=150x150`}/>
+        <h2> {props.monster.name} </h2>
+        <p> {props.monster.email} </p>
+    </div>
+)
